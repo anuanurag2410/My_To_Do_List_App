3dfa3ae06b4f58abf324edd9ed5242c253de57c9
Anurag Srivastava <anuanurag2410@gmail.com>
2023-06-28 19:51:31 +0530

    View engine Server Side Setup Done

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ const app=express();
 //Setup of View Engine
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
-
+app.get('/',async(req,res)=>{
+    try{
+        return res.render('home',{
+            title:'My To Do List'
+    });
+    }
+    catch(err)
+    {
+        console.log("Error n Fetching Details from DB",err);
+        return res.status(500).send('internal Server error ');
+    }
+});
 
 
 
